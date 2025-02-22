d4129c423bafd7ac7839cb90f41632a8268013c6
oyu <12345yuu6789@gmail.com>
2025-02-23 06:46:34 +0900

    docker環境で動くようになった

--- app/db/database.ts
+++ app/db/database.ts
@@ -5,7 +5,7 @@
 
 // const pool = new Pool({
 //   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
+//   // host: process.env.DB_HOST,
 //   database: process.env.DB_NAME,
 //   password: process.env.DB_PASSWORD,
 //   port: Number(process.env.DB_PORT),
